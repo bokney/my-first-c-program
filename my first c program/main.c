f7bb603ad383f19ae4305a325cf8b4542f6464ff
bokney <tokny@live.com>
2024-04-20 01:12:28 +0100

    Initial Commit

---
+++ my first c program/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  my first c program
+//
+//  Created by Cris Jarvis on 20/04/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
