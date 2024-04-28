2c30d6f7a6550dcc840d83f5cc8b05737368c69c
bokney <tokny@live.com>
2024-04-28 01:41:18 +0100

    huh? 🤔

---
+++ my first c program/objects.c
@@ -0,0 +1,8 @@
+//
+//  objects.c
+//  my first c program
+//
+//  Created by Cris Jarvis on 27/04/2024.
+//
+
+#include "objects.h"
---
+++ my first c program/objects.h
@@ -0,0 +1,13 @@
+//
+//  objects.h
+//  my first c program
+//
+//  Created by Cris Jarvis on 27/04/2024.
+//
+
+#ifndef objects_h
+#define objects_h
+
+#include <stdio.h>
+
+#endif /* objects_h */
