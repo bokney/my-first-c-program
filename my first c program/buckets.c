f21506dad39739d0db28f5fa93d663f2b005fa1c
bokney <tokny@live.com>
2024-04-30 03:23:18 +0100

    🎩 🦊

---
+++ my first c program/buckets.c
@@ -0,0 +1,8 @@
+//
+//  buckets.c
+//  my first c program
+//
+//  Created by Cris Jarvis on 30/04/2024.
+//
+
+#include "buckets.h"
---
+++ my first c program/buckets.h
@@ -0,0 +1,13 @@
+//
+//  buckets.h
+//  my first c program
+//
+//  Created by Cris Jarvis on 30/04/2024.
+//
+
+#ifndef buckets_h
+#define buckets_h
+
+#include <stdio.h>
+
+#endif /* buckets_h */
