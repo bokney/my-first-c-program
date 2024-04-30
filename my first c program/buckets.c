abf785eef4d5137a10a086e1fd3bce9ec8eb3084
bokney <tokny@live.com>
2024-04-30 03:25:05 +0100

    🌞

--- my first c program/buckets.c
+++ my first c program/buckets.c
@@ -1,8 +1,106 @@
-//
+
 //  buckets.c
 //  my first c program
-//
-//  Created by Cris Jarvis on 30/04/2024.
-//
+//  30/04/2024
 
+#include <stdlib.h>
 #include "buckets.h"
+#include "lists.h"
+
+struct bucket_ {
+    listNode *list;
+    void *(*create)(void);
+    void (*init)(void *data);
+    void (*destroy)(void *data);
+};
+
+
+bucket *bucketCreate(void) {
+    bucket *newBucket = (bucket *) malloc(sizeof(bucket));
+    if (newBucket == NULL) {
+        printf("error creating new bucket\n");
+        exit(1);
+    }
+    return newBucket;
+}
+
+void bucketInit(bucket *target) {
+    target->list = NULL;
+    target->create = NULL;
+    target->init = NULL;
+    target->destroy = NULL;
+}
+
+void bucketDestroy(bucket *target) {
+    if (target == NULL) {
+        printf("error - tried to destroy NULL bucket\n");
+        exit(1);
+    }
+    int length = listLength(target->list);
+    for (int i = 0; i < length; i++) {
+        free(listRemoveNode(&target->list, 0));
+    }
+}
+
+void bucketSetCreate(bucket *target, void *(*func)(void)) {
+    if (target == NULL) {
+        printf("error - tried to set create routine of a NULL bucket\n");
+        exit(1);
+    }
+    target->create = func;
+}
+
+void bucketSetInit(bucket *target, void (*func)(void *data)) {
+    if (target == NULL) {
+        printf("error - tried to set init routine of a NULL bucket\n");
+        exit(1);
+    }
+    target->init = func;
+}
+
+void bucketSetDestroy(bucket *target, void (*func)(void *data)) {
+    if (target == NULL) {
+        printf("error - tried to set destroy routine of a NULL bucket\n");
+        exit(1);
+    }
+    target->destroy = func;
+}
+
+void bucketSetRoutines(bucket *target,
+                       void *(*create)(void),
+                       void (*init)(void *data),
+                       void (*destroy)(void *data)) {
+    if (target == NULL) {
+        printf("error - tried to set routines of a NULL bucket\n");
+        exit(1);
+    }
+    target->create = create;
+    target->init = init;
+    target->destroy = destroy;
+}
+
+void *bucketRequest(bucket *target) {
+    if (target == NULL) {
+        printf("error - requested from NULL bucket\n");
+        exit(1);
+    }
+    void *data = NULL;
+    if (target->list == NULL) {
+        data = target->create;
+    } else {
+        data = listRemoveNode(&target->list, 0);
+    }
+    target->init(data);
+    return data;
+}
+
+void bucketReturn(bucket *target, void *data) {
+    if (target == NULL) {
+        printf("error - tried to return data to a NULL bucket\n");
+        exit(1);
+    } else if (data == NULL) {
+        printf("error - tried to return NULL data to a bucket\n");
+        exit(1);
+    }
+    listPrepend(&target->list, data);
+}
--- my first c program/buckets.h
+++ my first c program/buckets.h
@@ -1,13 +1,28 @@
-//
+
 //  buckets.h
 //  my first c program
-//
-//  Created by Cris Jarvis on 30/04/2024.
-//
+//  30/04/2024
 
 #ifndef buckets_h
 #define buckets_h
 
 #include <stdio.h>
+
+typedef struct bucket_ bucket;
+
+bucket *bucketCreate(void);
+void bucketInit(bucket *target);
+void bucketDestroy(bucket *target);
+
+void bucketSetCreate(bucket *target, void *(*func)(void));
+void bucketSetInit(bucket *target, void (*func)(void *data));
+void bucketSetDestroy(bucket *target, void (*func)(void *data));
+void bucketSetRoutines(bucket *target,
+                       void *(*create)(void),
+                       void (*init)(void *data),
+                       void (*destroy)(void *data));
+
+void *bucketRequest(bucket *target);
+void bucketReturn(bucket *target, void *data);
 
 #endif /* buckets_h */
